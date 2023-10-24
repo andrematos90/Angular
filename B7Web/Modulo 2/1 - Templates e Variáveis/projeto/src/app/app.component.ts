1652736586bec0ba82f585c589b09a6c87248600
André Matos <andrermatos90@gmail.com>
2023-10-24 20:01:48 -0300

    instalação, criação do projeto

---
+++ B7Web/Modulo 2/1 - Templates e Variáveis/projeto/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <h1> Meu próprio HTML</h1>
+   -o
+  `
+  
+  
+})
+export class AppComponent {
+  title = 'projeto';
+}
