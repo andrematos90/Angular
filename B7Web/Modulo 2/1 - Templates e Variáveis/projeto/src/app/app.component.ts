35626f8a45d11eb225e2d1c189e54eec22350abf
André Matos <andrermatos90@gmail.com>
2023-10-24 20:22:33 -0300

    templates e variáveis

--- B7Web/Modulo 2/1 - Templates e Variáveis/projeto/src/app/app.component.ts
+++ B7Web/Modulo 2/1 - Templates e Variáveis/projeto/src/app/app.component.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-  <h1> Meu próprio HTML</h1>
-   -o
-  `
+  template:
+  //primeira maneira de fazer o html
+   `<h1> prieira maneira de fazer o html</h1>`,
+
+  //segunda maneira de fazer o html
+  templateUrl: './app.component.html'
   
   
 })
 export class AppComponent {
-  title = 'projeto';
+  title = 'exemplo de variável';
+  nome = 'andré';
+  idade = '33';
+  nomes = ['andre', 'simba', 'collie', 'bin'];
+  ultimoNome = this.nomes[3];
 }
