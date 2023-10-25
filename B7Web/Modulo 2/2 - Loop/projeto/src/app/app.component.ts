4cf88d959164bc3abd3848561fc8bdca3b01552d
André Matos <andrermatos90@gmail.com>
2023-10-24 21:48:24 -0300

    loop ngFor

--- B7Web/Modulo 2/2 - Loop/projeto/src/app/app.component.ts
+++ B7Web/Modulo 2/2 - Loop/projeto/src/app/app.component.ts
@@ -2,9 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
+ 
 })
 export class AppComponent {
-  title = 'projeto';
+  title = 'como usar o ngFor';
+
+  carros = [
+    {modelo: 'Twingo', ano: 2001},
+    {modelo: 'Malibu', ano: 2010},
+    {modelo: 'New Civic', ano: 2008},
+    {modelo: 'Fusca', ano: 1985},
+    {modelo: 'Gol', ano: 2008}
+  ];
+
 }
