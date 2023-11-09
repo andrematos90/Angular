16fc49d3f088d35ac9a72b2b649e3f237b004829
André Matos <andrermatos90@gmail.com>
2023-11-09 07:34:57 -0300

    css no angular

---
+++ curso hora de codar/06 - CSS no Angular/projeto/src/app/components/fist-component/fist-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fist-component',
+  templateUrl: './fist-component.component.html',
+  styleUrls: ['./fist-component.component.css']
+})
+export class FistComponentComponent {
+
+  scoped:string = "CSS a nível de componente"
+
+}
