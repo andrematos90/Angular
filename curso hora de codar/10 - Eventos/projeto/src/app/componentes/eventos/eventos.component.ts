69a7526e159448d7d88474d565008a76378a7881
André Matos <andrermatos90@gmail.com>
2023-11-11 10:16:10 -0300

    eventos

---
+++ curso hora de codar/10 - Eventos/projeto/src/app/componentes/eventos/eventos.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.css']
+})
+export class EventosComponent {
+show:boolean = false;
+
+showMessage():void {
+  this.show = !this.show; // "!" faz com que sempre que clicado a variável tenha seu valor invertido, isso chama-se "toggle"
+
+}
+
+}
