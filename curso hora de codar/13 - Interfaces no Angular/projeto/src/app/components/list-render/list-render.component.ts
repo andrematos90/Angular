357e680a769e90e8b614e8cf0d1b016ce0e6a01b
André Matos <andrermatos90@gmail.com>
2023-11-13 15:40:22 -0300

    Interfaces

--- curso hora de codar/13 - Interfaces no Angular/projeto/src/app/components/list-render/list-render.component.ts
+++ curso hora de codar/13 - Interfaces no Angular/projeto/src/app/components/list-render/list-render.component.ts
@@ -1,3 +1,4 @@
+import { Animal } from './../../Animal';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-render.component.css']
 })
 export class ListRenderComponent {
+
+  animalDetails: string = '';
+
+  animais: Animal[] =[
+    {nome: 'Simba', raca: 'Rough collie', idade: 3},
+    {nome: 'Milu', raca: 'Vira lata', idade: 7},
+    {nome: 'Gatao', raca: 'gato', idade: 3},
+    {nome: 'luna', raca: 'Pastor', idade: 0 }
+  ]
+
+  showDetails(animal:Animal):void{
+    this.animalDetails = `${animal.nome} tem ${animal.idade} anos de idade e é da raça ${animal.raca}`
+  }
 
 }
