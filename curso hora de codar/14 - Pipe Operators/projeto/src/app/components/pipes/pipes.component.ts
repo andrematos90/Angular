093a3d64beb34aca1c46622b78ad7860b18c4504
André Matos <andrermatos90@gmail.com>
2023-11-15 08:06:15 -0300

    pipes

---
+++ curso hora de codar/14 - Pipe Operators/projeto/src/app/components/pipes/pipes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pipes',
+  templateUrl: './pipes.component.html',
+  styleUrls: ['./pipes.component.css']
+})
+export class PipesComponent {
+
+someText:string =   "textando com UMA Variável";
+today = new Date();
+}
