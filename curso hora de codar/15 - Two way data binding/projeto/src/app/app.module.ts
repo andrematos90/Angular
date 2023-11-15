47c863941b26b078fc55f605027b377fbc1c25ef
André Matos <andrermatos90@gmail.com>
2023-11-15 08:44:28 -0300

    two way data binding

---
+++ curso hora de codar/15 - Two way data binding/projeto/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TwoWayBindingComponent } from './components/two-way-binding/two-way-binding.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TwoWayBindingComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
