8cc0a6db1788868e6520a7f560158e5e0eaeb7b3
André Matos <andrermatos90@gmail.com>
2023-11-20 08:55:21 -0300

    criando intefaces

---
+++ curso hora de codar/27 - Criando Interfaces e configurando módulos/moments/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/page/about/about.component';
+import { HomeComponent } from './components/page/home/home.component';
+import { NewMomentComponent } from './components/page/new-moment/new-moment.component';
+import { MomentFormComponent } from './components/moment-form/moment-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    NewMomentComponent,
+    MomentFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
