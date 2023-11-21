03db9fe4b5ad2074fa18c44a63e88549d790dbea
André Matos <andrermatos90@gmail.com>
2023-11-21 11:54:36 -0300

    ..

---
+++ curso hora de codar/30 - Iniciando componente de mensagem/moments/src/app/components/messages/messages.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css']
+})
+export class MessagesComponent {
+  faTimes = faTimes;
+
+}
