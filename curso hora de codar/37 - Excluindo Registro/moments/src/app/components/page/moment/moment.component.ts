95f4085a8c519623bd66367b531c0b64a9e10348
André Matos <andrermatos90@gmail.com>
2023-11-23 10:36:41 -0300

    excluindo registro

--- curso hora de codar/37 - Excluindo Registro/moments/src/app/components/page/moment/moment.component.ts
+++ curso hora de codar/37 - Excluindo Registro/moments/src/app/components/page/moment/moment.component.ts
@@ -3,10 +3,13 @@ import { Moment } from 'src/app/Moment';
 import { Component, OnInit } from '@angular/core';
 import { MomentService } from 'src/app/services/moment.service';
 
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 
 import { faTimes, faEdit } from '@fortawesome/free-solid-svg-icons';
+import { MessagesService } from 'src/app/services/messages.service';
+
+
 
 
 
@@ -21,7 +24,12 @@ baseApiUrl = environment.baseApiUrl;
 faTimes = faTimes;
 faEdit = faEdit;
 
-constructor(private momentService: MomentService, private route: ActivatedRoute){}
+
+constructor(
+  private momentService: MomentService,
+  private route: ActivatedRoute,
+  private messageService: MessagesService,
+  private router: Router){}
 
 ngOnInit(): void {
     //id qe esta na url
@@ -30,4 +38,16 @@ ngOnInit(): void {
     this.momentService.getMoment(id).subscribe((item) => this.moment = item.data)
 
 }
+
+async removeHandler(id:number){
+  //chama o método no service para exluir e espera resposta da api voltar
+  await this.momentService.removeMoment(id).subscribe()
+
+  //mostra mensagem após
+  this.messageService.add("Momento Excluido com sucesso!")
+
+  //redireciona a págin após excluir
+  this.router.navigate(['/'])
+}
+
 }
