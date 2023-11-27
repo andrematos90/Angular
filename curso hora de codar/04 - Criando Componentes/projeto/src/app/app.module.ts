37ef32a464d48ac6560c615ca401bc96f4eaefa5
André Matos <andrermatos90@gmail.com>
2023-11-27 15:13:43 -0300

    criação de componentes

--- curso hora de codar/04 - Criando Componentes/projeto/src/app/app.module.ts
+++ curso hora de codar/04 - Criando Componentes/projeto/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponenteComponent } from './components/first-componente/first-componente.component';
+import { SecondComponentComponent } from './components/second-component/second-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponenteComponent
+    FirstComponenteComponent,
+    SecondComponentComponent
   ],
   imports: [
     BrowserModule,
