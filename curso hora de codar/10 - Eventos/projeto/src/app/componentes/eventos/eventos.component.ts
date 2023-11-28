82b532195b570574b27597a748ed59c06b54d24c
André Matos <andrermatos90@gmail.com>
2023-11-28 10:41:25 -0300

    eventos no angular

--- curso hora de codar/10 - Eventos/projeto/src/app/componentes/eventos/eventos.component.ts
+++ curso hora de codar/10 - Eventos/projeto/src/app/componentes/eventos/eventos.component.ts
@@ -9,7 +9,7 @@ export class EventosComponent {
 show:boolean = false;
 
 showMessage():void {
-  this.show = !this.show; // "!" faz com que sempre que clicado a variável tenha seu valor invertido, isso chama-se "toggle"
+  this.show = !this.show; /* "!" faz com que sempre que clicado a variável tenha seu valor invertido, isso chama-se "toggle" e evita a necessidade de criar outro método para voltar o valor da váriavel novamente*/
 
 }
 
