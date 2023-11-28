240eca72c48383d1af1a9bec7a9c9e488f37532a
André Matos <andrermatos90@gmail.com>
2023-11-28 09:59:51 -0300

    diretivas de estilo

--- curso hora de codar/08 -  Diretivas no Angular/projeto/src/app/components/directives/directives.component.ts
+++ curso hora de codar/08 -  Diretivas no Angular/projeto/src/app/components/directives/directives.component.ts
@@ -6,11 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./directives.component.css']
 })
 export class DirectivesComponent {
+
+  //propriedades que serão usadas no template
   color = 'yellow';
   font = 'Arial'
   size = '60px'
 
+  //propriedade uma lista de classes que serão definidas no arquuivo .css
   classes = ['green-title', 'small-title'];
+
+  //propriedade com uma classe só
   underline = 'underline-title'
 
 }
