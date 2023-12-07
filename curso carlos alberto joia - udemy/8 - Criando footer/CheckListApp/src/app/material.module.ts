545c095a56d923d203564da3c66946ea17bf1f39
André Matos <andrermatos90@gmail.com>
2023-12-07 07:27:40 -0300

    criação do header

---
+++ curso carlos alberto joia - udemy/8 - Criando footer/CheckListApp/src/app/material.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    MatToolbarModule
+
+  ],
+  exports:[
+    MatToolbarModule
+  ],
+
+  providers: [],
+  bootstrap: []
+})
+export class MaterialModule { }
