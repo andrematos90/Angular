59596b426757636c8f3c9970f59f0dc755de3d55
André Matos <andrermatos90@gmail.com>
2023-12-08 08:55:17 -0300

    rotas aninhadas

---
+++ curso carlos alberto joia - udemy/11- Rotas Aninhadas e Guards/CheckListApp/src/app/material.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+
+
+
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatSidenavModule,
+    MatCheckboxModule,
+    FormsModule,
+    MatListModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatDividerModule
+
+  ],
+  exports:[
+    MatButtonModule,
+    MatSidenavModule,
+    MatCheckboxModule,
+    FormsModule,
+    MatListModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatDividerModule
+
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class MaterialModule { }
