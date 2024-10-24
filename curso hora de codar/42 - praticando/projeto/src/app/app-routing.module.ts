bf68a9082631da54d134376c5edb51f1011676e8
André Matos <andrerosadematos90@gmail.com>
2024-10-24 11:12:03 -0300

    router

--- curso hora de codar/42 - praticando/projeto/src/app/app-routing.module.ts
+++ curso hora de codar/42 - praticando/projeto/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListRenderComponent } from './components/list-render/list-render.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '/', component: ListRenderComponent
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
