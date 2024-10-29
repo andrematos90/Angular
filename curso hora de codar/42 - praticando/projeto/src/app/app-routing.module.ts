9cb7b3946a4c8226ebbfcb2bce4c8465bc4cefe8
André Matos <andrerosadematos90@gmail.com>
2024-10-29 09:41:04 -0300

    router

--- curso hora de codar/42 - praticando/projeto/src/app/app-routing.module.ts
+++ curso hora de codar/42 - praticando/projeto/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListRenderComponent } from './components/list-render/list-render.component';
+import { RenderizacaoCondicionalComponent } from './components/renderizacao-condicional/renderizacao-condicional.component'
 
 const routes: Routes = [
-  {path: '/', component: ListRenderComponent
+  { path: '', component: ListRenderComponent },
+  { path: 'render', component: RenderizacaoCondicionalComponent }
 
-  }
 ];
 
 @NgModule({
